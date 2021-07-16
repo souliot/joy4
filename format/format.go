f6e5c94b06b48c3b9f08dbfb3bffe897b3438d08
souliot <linleizhou19910924@163.com>
2021-07-16 13:19:58 +0800

    init

---
+++ format/format.go
@@ -0,0 +1,21 @@
+package format
+
+import (
+	"github.com/souliot/joy4/format/mp4"
+	"github.com/souliot/joy4/format/ts"
+	"github.com/souliot/joy4/format/rtmp"
+	"github.com/souliot/joy4/format/rtsp"
+	"github.com/souliot/joy4/format/flv"
+	"github.com/souliot/joy4/format/aac"
+	"github.com/souliot/joy4/av/avutil"
+)
+
+func RegisterAll() {
+	avutil.DefaultHandlers.Add(mp4.Handler)
+	avutil.DefaultHandlers.Add(ts.Handler)
+	avutil.DefaultHandlers.Add(rtmp.Handler)
+	avutil.DefaultHandlers.Add(rtsp.Handler)
+	avutil.DefaultHandlers.Add(flv.Handler)
+	avutil.DefaultHandlers.Add(aac.Handler)
+}
+
